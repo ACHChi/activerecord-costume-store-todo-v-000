1ccd487b240d896cfcc34419ca0d2b8ff6f91337
Chao Hsuan Chi <allen0413@gmail.com>
2019-03-19 02:32:52 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,11 +6,12 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.string :theme
-      t.integer :price
+      t.float :price
       t.boolean :family_friendly
       t.datetime :opening
       t.datetime :closing
       t.text :description
+      t.timestamps null: false
     end
   end
 
