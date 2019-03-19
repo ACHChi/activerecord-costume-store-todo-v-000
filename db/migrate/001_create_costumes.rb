f76231b4708a418359e85e628b233e750cbe09f5
Chao Hsuan Chi <allen0413@gmail.com>
2019-03-19 02:30:00 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.text :size
+      t.string :size
       t.string :image_url
       t.timestamps
     end
