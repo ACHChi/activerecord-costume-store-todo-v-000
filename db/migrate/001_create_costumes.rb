d0ec1e261ad605a814f3a9dadd39222deea01a7a
Chao Hsuan Chi <allen0413@gmail.com>
2019-03-19 02:30:15 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,7 +7,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :size
       t.string :image_url
-      t.timestamps
+      t.timestamps mull:false
     end
   end
 
